e627802e4912f33ccdc4de42d9beacdddc4577b6
italo <italogithub@gmail.com>
2023-03-18 13:10:08 -0300

    aprendendo sobre tipagem no TS

---
+++ Modulo1/Aula2-Tipagem/tipos.ts
@@ -0,0 +1,6 @@
+let tipoString: string = "Declarando tipo string"
+let tipoBoolean: boolean = true
+let tipoNumber: number = 23
+
+
+console.log(tipoString, tipoBoolean, tipoNumber)
--- Modulo1/FirstProgram/hello.ts
+++ Modulo1/FirstProgram/hello.ts
@@ -4,3 +4,4 @@ console.log(mensagem)
 
 
 
+
