320ec83b6cfe31c4d67ac62b4e9be5c5cc78cbad
italo <italogithub@gmail.com>
2023-03-18 13:32:05 -0300

    trabalhando com função

--- Modulo1/Aula2-Tipagem/TypeAnnotations.ts
+++ Modulo1/Aula2-Tipagem/TypeAnnotations.ts
@@ -26,3 +26,11 @@ let carro: {
 carro = {nome: "Ferraro", ano: 2022, preco: 80000}
 
 console.log(carro);
+
+// ====> Functions [Type Annotations]
+
+function multiplicarNumeros(num1: number, num2: number){
+    return num1 * num2;
+}
+
+console.log (multiplicarNumeros(2, 3))
