9a71c3f5947967d083e9b50d0687c6b3d169cb49
italo <italogithub@gmail.com>
2023-03-20 15:16:03 -0300

    utilizando bigint

---
+++ Modulo1/Aula2-Tipagem/BigintAndNumber.js
@@ -0,0 +1,26 @@
+// ======> exemplo Number
+var num1 = 23.0;
+var num2 = 0x7BCF;
+var num3 = 383;
+var num4 = 49;
+console.log("Número Ponto Flutuante: " + num1);
+console.log(typeof (num1));
+console.log("Número Hexadecimal: " + num2);
+console.log(typeof (num2));
+console.log("Número Octadecimal: " + num3);
+console.log(typeof (num3));
+console.log("Número Binário: " + num4);
+console.log(typeof (num4));
+// ======> exemplo com BigInt
+var num5 = 254535345676545645n;
+var num6 = 2324455511122n;
+var num7 = 2131211455556644431232155n;
+var num8 = 2131212213213213921842183213821832813281n;
+console.log("Número em BigInt é: " + num5);
+console.log(typeof (num5));
+console.log("Número em BigInt é: " + num6);
+console.log(typeof (num6));
+console.log("Número em BigInt é: " + num7);
+console.log(typeof (num7));
+console.log("Número em BigInt é: " + num8);
+console.log(typeof (num8));
---
+++ Modulo1/Aula2-Tipagem/BigintAndNumber.ts
@@ -0,0 +1,46 @@
+
+
+// ======> exemplo Number
+
+let num1: number = 23.0;
+let num2: number = 0x7BCF;
+let num3: number = 0o577;
+let num4: number = 0b110001;
+
+console.log("Número Ponto Flutuante: " + num1);
+console.log(typeof(num1));
+console.log("Número Hexadecimal: " + num2);
+console.log(typeof(num2));
+console.log("Número Octadecimal: " + num3);
+console.log(typeof(num3));
+console.log("Número Binário: " + num4);
+console.log(typeof(num4));
+
+
+
+// ======> exemplo com BigInt
+
+
+let num5: bigint = 254535345676545645n;
+let num6: bigint = 2324455511122n;
+let num7: bigint = 2131211455556644431232155n;
+let num8: bigint = 2131212213213213921842183213821832813281n;
+
+console.log("Número em BigInt é: " + num5);
+console.log(typeof(num5));
+console.log("Número em BigInt é: " + num6);
+console.log(typeof(num6));
+console.log("Número em BigInt é: " + num7);
+console.log(typeof(num7));
+console.log("Número em BigInt é: " + num8)
+console.log(typeof(num8));
+
+
+
+
+
+
+
+
+
+
