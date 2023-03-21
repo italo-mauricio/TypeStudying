f76a4dbaa7a814604c4546274f259f17efa21912
italo <italogithub@gmail.com>
2023-03-21 13:50:32 -0300

    arrays

---
+++ Modulo1/Aula3-Arrays/Array.js
@@ -0,0 +1,4 @@
+var exemploArray = ['maçã', 'pêra', 'abacaxi'];
+console.log(exemploArray);
+var exemploArrayObject = ['feijão', 'arroz', 'carne'];
+console.log(exemploArray[2]);
---
+++ Modulo1/Aula3-Arrays/Array.ts
@@ -0,0 +1,18 @@
+
+
+// ======> Uso de Colchetes:
+
+let exemploArray: string[] = ['maçã', 'pêra', 'abacaxi'];
+console.log(exemploArray[2]);
+
+
+// ======> Uso de Array Object
+
+let exemploArrayObject: Array <string> = ['feijão', 'arroz', 'carne'];
+console.log(exemploArray[2]);
+
+
+
+
+
+
