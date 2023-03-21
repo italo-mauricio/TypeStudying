cd9dbcf22dcbfe1837788a98fad558215c73b37d
italo <italogithub@gmail.com>
2023-03-21 13:21:42 -0300

    finalizando strings

--- Modulo1/Aula2-Tipagem/StringEmTs.js
+++ Modulo1/Aula2-Tipagem/StringEmTs.js
@@ -2,3 +2,13 @@ var meuNome = "italo";
 var nomeFaculdade = "UFRN";
 var detalhes = "".concat(meuNome, " estuda na ").concat(nomeFaculdade);
 console.log("Informações: " + detalhes);
+//==============================================================>
+// Single Quotes
+var nomeCompleto = 'Italo Mauricio';
+console.log(nomeCompleto);
+// Double Quotes
+var nomeCurso = "Sistemas de Informação";
+console.log(nomeCurso);
+// Back Ticks
+var informacoes = "O aluno ".concat(nomeCompleto, " que faz parte da ").concat(nomeFaculdade, " no curso de ").concat(nomeCurso, " est\u00E1 aprovado!");
+console.log(informacoes);
--- Modulo1/Aula2-Tipagem/StringEmTs.ts
+++ Modulo1/Aula2-Tipagem/StringEmTs.ts
@@ -4,3 +4,24 @@ let nomeFaculdade: string = "UFRN";
 let detalhes: string = `${meuNome} estuda na ${nomeFaculdade}`;
 console.log("Informações: " + detalhes);
 
+
+//==============================================================>
+// Single Quotes
+
+let nomeCompleto: string = 'Italo Mauricio';
+console.log(nomeCompleto);
+
+// Double Quotes
+
+let nomeCurso: string = "Sistemas de Informação";
+console.log(nomeCurso);
+
+// Back Ticks
+
+let informacoes: string = `O aluno ${nomeCompleto} que faz parte da ${nomeFaculdade} no curso de ${nomeCurso} está aprovado!`;
+console.log(informacoes);
+
+
+
+
+
