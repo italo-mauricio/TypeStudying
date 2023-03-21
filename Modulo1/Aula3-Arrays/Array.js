288e4d684b45afd66ccef894eba9b467ae6eac24
italo <italogithub@gmail.com>
2023-03-21 17:54:20 -0300

    update

--- Modulo1/Aula3-Arrays/Array.js
+++ Modulo1/Aula3-Arrays/Array.js
@@ -1,4 +1,26 @@
+// ======> Uso de Colchetes:
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var exemploArray = ['maçã', 'pêra', 'abacaxi'];
-console.log(exemploArray);
+console.log(exemploArray[2]);
+// ======> Uso de Array Object
 var exemploArrayObject = ['feijão', 'arroz', 'carne'];
 console.log(exemploArray[2]);
+// ======> Adicionando mais strings com método 'push'
+var idiomas = ['Português', 'Inglês', 'Espanhol'];
+console.log(idiomas);
+idiomas.push('Árabe'); // adiciona no final do array um novo elemento e retorna a posição
+console.log(idiomas);
+// ======> Usando o Length
+var idiomas1 = ['Português', 'Inglês', 'Espanhol'];
+console.log("O tamanho do Array é de: " + idiomas1.length);
+// ======> Utilizando o Spread Operator
+var listaNum = [0, 1, 2, 3, 4, 5];
+listaNum = __spreadArray(__spreadArray([], listaNum, true), [6, 7, 8, 9, 10], false);
--- Modulo1/Aula3-Arrays/Array.ts
+++ Modulo1/Aula3-Arrays/Array.ts
@@ -11,8 +11,33 @@ console.log(exemploArray[2]);
 let exemploArrayObject: Array <string> = ['feijão', 'arroz', 'carne'];
 console.log(exemploArray[2]);
 
+// ======> Adicionando mais strings com método 'push'
 
+let idiomas: Array<string> = ['Português', 'Inglês', 'Espanhol'];
+console.log(idiomas);
+idiomas.push('Árabe');    // adiciona no final do array um novo elemento e retorna a posição
+console.log(idiomas);
 
+// ======> Usando o Length
 
+let idiomas1: Array<string> = ['Português', 'Inglês', 'Espanhol'];
+console.log("O tamanho do Array é de: " + idiomas1.length);
 
+// ======> Utilizando o Spread Operator
 
+let listaNum: Array<number> = [0, 1, 2, 3, 4, 5];
+listaNum = [...listaNum, 6, 7, 8, 9, 10]; 
+console.log(listaNum);
+
+// ======> Array com laço de iteração
+
+let linguagemArray: string[] = new Array('Python', 'Java', 'JavaScript', 'GoLang');
+
+function funcaoLinguagem(linguaguens: string[]){
+    for (let i = 0; i < linguagemArray.length; i++){
+        console.log(linguagemArray[i]);
+        
+    }
+}
+
+funcaoLinguagem(linguagemArray);
